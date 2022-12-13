6921cd3552328e1fc48a79d56807938dd227c72d
Roland Meier <roland@cloudastro.de>
2022-12-13 13:07:10 +0100

    Changed React Homepage to CloudAstro MicroHack.

--- 01-build-your-modern-web-application/cloudastro-reactapp/src/App.js
+++ 01-build-your-modern-web-application/cloudastro-reactapp/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Welcome to the CLoudAstro MicroHack.
+          Welcome to the CloudAstro MicroHack.
         </p>
         <a
           className="App-link"
